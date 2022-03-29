0db194ba7adc2e285d3d935a72c5166df61e6bfd
Christopher Foose <chrisfoose@gmail.com>
2022-03-29 13:11:10 -0700

    prepend

---
+++ prependToString.js
@@ -0,0 +1,7 @@
+// prepending a string to another string
+
+let firstPart = prompt('Enter the first part');
+let secondPart = prompt('Enter the second part');
+
+fullString = 'Here is the prepended string: ' + secondPart + firstPart;
+console.log(fullString);
---
+++ prepentToString.js
@@ -0,0 +1,8 @@
+// prepending a string to another string
+
+let firstPart = prompt('Enter the first part');
+let secondPart = prompt('Enter the second part');
+
+fullString = 'Here is the prepended string: ' + secondPart + '&nbsp &nbsp' + firstPart;
+console.log(fullString);
+window.alert(fullString);
